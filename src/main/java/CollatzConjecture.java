c6e534cd1acb7909c2421d6bd492dd0fecb59d22
itayventura <ventura.itay@gmail.com>
2022-05-12 21:57:51 +0300

    validating out of bounds numbers with big integer. algorithm can now run to infinity.

--- src/main/java/CollatzConjecture.java
+++ src/main/java/CollatzConjecture.java
@@ -1,27 +1,48 @@
+import java.math.BigInteger;
+
 public class CollatzConjecture {
 
     public static void validateCollatzNumber(long n){
         long m = n;
         while (m >= n){
             if((m&1) == 1) { //ODD
-                m = m*3 + 1;
+                m = m*3+1;
             } else{ //even
                 m = m>>1;
             }
             if(m < 0){
-                System.out.println(m + " is out of bounds and bigger than the upper limit " + Long.MAX_VALUE + ". we reached: " + n);
-                throw new NullPointerException();
+                System.out.println(m + " is out of bounds and bigger than the upper limit " + Long.MAX_VALUE + ". initial number: " + n);
+                validateCollatzNumber(BigInteger.valueOf(n));
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static boolean isOdd(BigInteger val) {
+        return !val.mod(new BigInteger("2")).equals(BigInteger.ZERO);
+    }
 
-        for(long i = 2; i<= 8528817511L; i++){
-            validateCollatzNumber(i);
-            if((i & 16777215L) == 0) { //print every x numbers
-                System.out.println(i);
+    public static void validateCollatzNumber(BigInteger n){
+        System.out.println("validating that " + n + " is a collatz number with BigInteger");
+        BigInteger m = n;
+        while (m.compareTo(n) >= 0){
+            if(isOdd(m)){
+                m = m.multiply(BigInteger.valueOf(3)).add(BigInteger.ONE);
+            } else{
+                m = m.divide(BigInteger.valueOf(2));
             }
         }
+        System.out.println(n + " is a collatz number");
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        for(long i = 2; i<= 20000000000L; i++){
+            validateCollatzNumber(i);
+            if((i & 16777215L) == 0) { //print every x numbers
+                System.out.println("number: " +  i + " time: " + (System.currentTimeMillis()-startTime));
+            }
+        }
+        System.out.println("algorithm took: " + (System.currentTimeMillis()-startTime));
     }
 }
